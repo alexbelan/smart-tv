d75f9ab14ab4272bef36432543f1174835259afd
Alex Belan <aleks.belan2000@gmail.com>
2022-02-09 15:25:06 +0500

    added player tracking

--- src/components/ViewVideo.tsx
+++ src/components/ViewVideo.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactPlayer, { ReactPlayerProps } from 'react-player'
 // import './App.css';
-import ForestVideo from "../assets/Forest.mp4";
+// import ForestVideo from "../assets/Forest.mp4";
 
 const ViewVideo: React.FC = () => {
+
+  const [showBanner, setShowBanner] = useState<boolean>(false)
+
+  const progressPlayr = (event: ReactPlayerProps) => {
+    if (event.playedSeconds >= 5 && !showBanner) {
+      setShowBanner(true)
+    }
+  }
+
   return (
-    <video id="background-video" loop autoPlay={true}>
-        <source src={ForestVideo} type="video/mp4" />
-        {/* <source src={this.state.videoURL} type="video/ogg" /> */}
-        {/* Your browser does not support the video tag. */}
-    </video>
+    <ReactPlayer 
+      url='https://www.youtube.com/watch?v=M7FIvfx5J10'
+      loop={true}
+      controls={true}
+      width={"100%"}
+      height={"720px"}
+      onProgress={progressPlayr}
+     />
     );
 }
 
